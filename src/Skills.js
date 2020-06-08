15e68c81a2fe8563a3e0fa3f0a13eb11779dfcb6
Ali Hassan <alyhassan862@gmail.com>
2020-06-08 18:02:33 +0000

    added about

--- src/Skills.js
+++ src/Skills.js
@@ -1,5 +1,7 @@
 import React,{Component} from 'react'
-
+import bracketopen from './bracketopen.png'
+import bracketclose from './bracketclose.png'
+import Fade from 'react-reveal'
 
 
 
@@ -7,12 +9,17 @@ import React,{Component} from 'react'
 export default class Skills extends Component{
     render(){
         return(
-            <div className="cover">
-                <h1 style={{
-                    color:'white',
-                    textAlign:'center',
-                    
-                }}>Page 2</h1>
+            <div className="cover2">
+                <img className="open" src={bracketopen} alt="open"/>
+                <h1 className="about">About Me,</h1>
+                <Fade bottom><p>I am a Frontend Web Developer with Experience in 
+                working with New Frontend Technologies and Looking forward to add Backend 
+                and Cloud Native Technologies to my developers Stack.
+                Highly Passionate About Applying and Learning DL/ML on Cloud Native Mobile & Webs.
+                Exploring IoT and Highly Interested in Integrating these Technologies into Single 
+                Product.  </p></Fade>
+                <img className="close" src={bracketclose} alt="close"/>
+
             </div>
         )
     }
